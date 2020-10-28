d1a22e71061297fb98d65c555c008417de6d683f
Ulima Inas Shabrina <nanaswarnakuning@gmail.com>
2020-10-28 16:30:19 +0700

    Refactoring SignIn Register

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SignIn from './SignIn';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignIn />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
