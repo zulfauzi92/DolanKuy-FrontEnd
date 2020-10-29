77a7e3443765cb079c07d662aa8d610e0a661785
Ulima Inas Shabrina <nanaswarnakuning@gmail.com>
2020-10-29 21:15:41 +0700

    Refactoring

--- src/Pages/Auth/SignIn.js
+++ src/Pages/Auth/SignIn.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './Auth.css';
 import "bootstrap/dist/css/bootstrap.css";
-import {Row, Col} from 'react-bootstrap';
-import LeftSide from "./RightSide";
+import {Row, Col, Form, Button} from 'react-bootstrap';
+import LeftSide from "./LeftSide";
 import NavbarTop from "../../Components/NavbarTop";
-import RightSide from "./LeftSide";
 
 function SignIn() {
-  return (
-    <div className="App">
-      <NavbarTop />
-      <Row className="landing">
-        <Col ><RightSide /></Col>
-        <Col ><LeftSide /></Col>
-      </Row>
-    </div>
-  );
+    return (
+      <div className="App">
+        <NavbarTop />
+        <Row className="landing">
+            <Col ><LeftSide /></Col>
+            <Col ><br/>
+                <br/>
+                <br/>
+                <Form style={{width:"80%", marginLeft:"10%", marginTop:"10%"}}>
+                    <Form.Group>
+                        <Form.Label>Enter your email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter your email" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Enter your password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter your password" />
+                    </Form.Group>
+                    <br/>
+                    <Button variant="primary" size="md" block>
+                        Block level button
+                    </Button>
+                </Form>
+            </Col>
+        </Row>
+      </div>
+    );
 }
 
 export default SignIn;
