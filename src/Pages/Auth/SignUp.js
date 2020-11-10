e0ab56d28adb00d7a9a1102166657ce8e71bd2d9
Ulima Inas Shabrina <nanaswarnakuning@gmail.com>
2020-11-10 09:48:47 +0700

    Sign In Sign Up Updated

--- src/Pages/Auth/SignUp.js
+++ src/Pages/Auth/SignUp.js
@@ -3,12 +3,10 @@ import './Auth.css';
 import "bootstrap/dist/css/bootstrap.css";
 import {Row, Col, Form, Button, InputGroup} from 'react-bootstrap';
 import LeftSide from "./LeftSide";
-import NavbarTop from "../../Components/NavbarTop";
 
 function SignUp() {
     return (
       <div className="App">
-        <NavbarTop />
         <Row className="landing">
             <Col><LeftSide /></Col>
             <Col xs lg="5">
